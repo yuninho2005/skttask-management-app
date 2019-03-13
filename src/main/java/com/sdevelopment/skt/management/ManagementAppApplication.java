30dc616378a1e68cd30eb274ee18df065302af27
Yunior Alayo <yalayo@NSL-YALAYOs-MacBook-Pro.local>
2019-03-13 12:02:34 -0700

    Initial commit with fresh project.

---
+++ src/main/java/com/sdevelopment/skt/management/ManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.sdevelopment.skt.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sdevelopment/skt/management/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sdevelopment.skt.management;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ManagementAppApplication.class);
+    }
+
+}
